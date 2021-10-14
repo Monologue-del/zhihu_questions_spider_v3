6afb0bd7a9deb1ee6deea25a733e8b39334433c6
Monologue-del <51441621+Monologue-del@users.noreply.github.com>
2021-10-14 08:22:23 +0800

    Add files via upload
    更新从MongoDB获取已爬取的问题id

--- answers/answers/spiders/mySpider.py
+++ answers/answers/spiders/mySpider.py
@@ -1,26 +1,31 @@
 import scrapy
 import datetime
 import time
+import pymongo
+from answers.settings import MONGO_URI
 from answers.settings import DATETIME_FORMAT
 from answers.items import AnswersItem
-from lxml import etree
 import json
 
 
 class MyspiderSpider(scrapy.Spider):
     name = 'mySpider'
     # allowed_domains = ['www.xxx.com']
-    start_urls = ['http://www.xxx.com/']
-    # 待完善：从已爬取的问题数据中获取问题id列表
-    question_id_list = ['360855944', '351797006']
+
+    # 从MongoDB获取问题id
+    myClient = pymongo.MongoClient(MONGO_URI)
+    myDB = myClient['zhihu_Questions']
+    myCol = myDB['questions_url']
+    question_id_list = myCol.find({}, {'_id': 0, 'id': 1})
+
     answer_url = 'http://www.zhihu.com/api/v4/questions/{question_id}/answers?include=data%5B*%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cattachment%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Cis_labeled%2Cpaid_info%2Cpaid_info_content%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_recognized%3Bdata%5B*%5D.mark_infos%5B*%5D.url%3Bdata%5B*%5D.author.follower_count%2Cbadge%5B*%5D.topics%3Bdata%5B*%5D.settings.table_of_content.enabled&offset=&limit=10&sort_by=default&platform=desktop'
 
     def start_requests(self):
         for question_id in self.question_id_list:
-            yield scrapy.Request(self.answer_url.format(question_id=question_id), headers={
+            yield scrapy.Request(self.answer_url.format(question_id=question_id['id']), headers={
                 'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'},
                                  callback=self.parse_answer)
-            time.sleep(3)
+            time.sleep(1)
 
     def parse_answer(self, response):
         """
